b551f5dee6223d0d6b52e7486588387e75aed76b
thothcher <g.vakhtangishvili@itstep.ge>
2024-09-27 15:49:53 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,143 @@
+;
+
+let image6 = [
+    [
+
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'red', 'red', 'red', 'black', 'black', 'red', 'red', 'red', 'black', 'black',
+        'black', 'black', 'black', 'red', 'black', 'black', 'black', 'black', 'red', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'red', 'black', 'black', 'black', 'black', 'red', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'red', 'red', 'red', 'red', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'red', 'red', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black'
+    ], [
+
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black'
+    ],
+    [
+
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'red', 'black', 'black', 'black', 'black', 'red', 'black', 'black', 'black',
+        'black', 'black', 'red', 'red', 'red', 'black', 'black', 'red', 'red', 'red', 'black', 'black',
+        'black', 'black', 'black', 'red', 'black', 'black', 'black', 'black', 'red', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'red', 'black', 'black', 'black', 'black', 'red', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'red', 'red', 'red', 'red', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black'
+    ],
+    [
+
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black'
+    ],
+    [
+
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'red', 'red', 'red', 'black', 'black', 'red', 'red', 'red', 'black', 'black',
+        'black', 'black', 'black', 'red', 'black', 'black', 'black', 'black', 'red', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'red', 'black', 'black', 'black', 'black', 'red', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'red', 'red', 'red', 'red', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'red', 'red', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black',
+        'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black'
+    ]
+]
+//get div element from html with class .frame
+//divide it with grid as many colums as many dimension has image(each color represents as column)
+let frame = document.querySelector('.frame')
+
+
+
+function createFrame(num) {
+    frame.style.gridTemplateColumns = `repeat( ${num}, 1fr)`
+    frame.style.width = `${num * 20}px`
+    for (let i = 0; i < num * num; i++) {
+        let px = document.createElement('div')
+        frame.appendChild(px)
+    }
+}
+
+function render(image) {
+    var pxs = frame.children
+    for (let i = 0; i < image.length; i++) {
+        setTimeout(() => {
+            for (let j = 0; j < image[i].length; j++) {
+                pxs[j].style.backgroundColor = image[i][j]
+                console.log(image[i][j])
+            }
+
+        }, `${i}000`);
+
+    }
+}
+createFrame(12)
+render(image6)
+
+
+
+
+/////////////////////////////-------old version--------///////////////////////////////////////////
+// frame.style.gridTemplateColumns = `repeat( ${image1[0][0].length}, 1fr)`
+// frame.style.width = `${image1[0][0].length * 20}px`
+// frame.innerHTML = ''
+
+// function gen(layer) {
+//     for (let row of layer) {
+//         //loop to get every color of image row
+//         for (let color of row) {
+//             //create div with 10px width and 10px height
+//             let px = document.createElement('div')
+//             //give it backgruond color from image array
+//             px.style.backgroundColor = color
+//             //add it to the frame as a child element
+//             frame.appendChild(px)
+//         }
+//     }
+// }
+
+//render function which calls gen function with interval to make like animation
+// function render(image) {
+//     for (let i = 0; i < image.length; i++) {
+//         setTimeout(() => {
+//             gen(image[i])
+//         }, `${i}000`);
+//     }
+// }
+
+
+// render(image1)
+
